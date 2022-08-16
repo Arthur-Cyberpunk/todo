8efea5fb62d743d23211c89a3e6339a622875079
Arthur Gabriel dos Santos <santos.santos86@hotmail.com>
2022-08-16 15:06:57 -0300

    created list of tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
 import * as C from "./App.style";
+import ListItem from "./components/ListItem";
+import { Item } from "./types/Item";
 const App = () => {
+  const [list, setList] = useState<Item[]>([]);
   return (
     <C.Container>
       <C.Area>
         <C.Header>Lista de Tarefas</C.Header>
+
+        {list.map((item, index) => (
+          <ListItem key={index} item={item} />
+        ))}
       </C.Area>
     </C.Container>
   );
